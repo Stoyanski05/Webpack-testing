bf29d6c9e66ce09d6da30e47e1a56bb0adbd3b6a
Stoyanski <danchevstoyan@gmail.com>
2023-10-13 11:07:23 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const path = require('path')
+ 
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: "asset",
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/img'), to: path.resolve(__dirname, 'dist/img') },
+            ]
+        })
+    ],
+    optimization: {
+        minimizer: [
+            "...",
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.sharpMinify,
+                }
+            })
+        ]
+    },
+    devtool: "source-map"
+};
